3bd8ed90d1e1f894bf04c160e2a3a2e198036a24
arjunhunt <arjunsingh090730@gmail.com>
2026-08-26 10:38:17 +0530

    Set dark palette colors as primary in Color.kt

--- app/src/main/java/com/example/pawalert/ui/theme/Color.kt
+++ app/src/main/java/com/example/pawalert/ui/theme/Color.kt
@@ -7,9 +7,16 @@ val Amber40 = Color(0xFFEF6C00)
 val Brown80 = Color(0xFFD7CCC8)
 val Brown40 = Color(0xFF5D4037)
 
-val StatusOpen = Color(0xFFD32F2F) // red - needs help
-val StatusInProgress = Color(0xFFF9A825) // amber - being handled
-val StatusResolved = Color(0xFF388E3C) // green - resolved
+val StatusOpen = Color(0xFFE53935)       // Red - needs help
+val StatusInProgress = Color(0xFFFFB300) // Amber - being handled
+val StatusResolved = Color(0xFF43A047)   // Green - resolved
 
-val Background = Color(0xFFFFFBF5)
-val Surface = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF141210)
+val DarkSurface = Color(0xFF1F1B18)
+val DarkSurfaceVariant = Color(0xFF2C2723)
+
+val Background = DarkBackground
+val Surface = DarkSurface
+val SurfaceVariant = DarkSurfaceVariant
+val OnBackground = Color(0xFFF5EFEB)
+val OnSurface = Color(0xFFF5EFEB)
